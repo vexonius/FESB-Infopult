a94ebe8b23f182a5ece8e034207b685260b8d638
etino7 <tinoet@ymail.com>
2020-04-27 18:09:21 +0200

    updated dependencies

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,8 +1,14 @@
 module.exports = {
-    async getQueryResults(req, res) {
-        res.status(200).json('All query results');
-    },
-     async info(req, res) {
-        res.status(200).json("Infopult");
-     }
-}
+  async getQueryResults(req, res) {
+    return res.status(200).json('All query results')
+  },
+  async info(req, res) {
+    return res.status(200).json('Infopult')
+  },
+  async getAllStaff(req, res) {
+    return res.status(200).json('osobe')
+  },
+  async getPerson(req, res) {
+    return res.status(200).json('osoba')
+  }
+};
--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 /* eslint-disable linebreak-style */
-const express = require('express');
-const controller = require('./controllers/controller');
+const express = require('express')
+const controller = require('./controllers/controller')
 
 const PORT = 8081 || process.env.PORT;
 
 const app = express();
 
-app.get('/', controller.info);
+app.get('/', controller.info)
+app.get("/osobe", controller.getAllStaff);
+app.get("/osobe/:id", controller.getPerson);
+app.get('/naslovna', controller.info)
 
-app.listen(PORT, () => { console.log(`Started listening on port ${PORT}`); });
+app.listen(PORT, () => { console.log(`Started listening on port ${PORT}`); })
